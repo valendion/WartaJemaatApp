ea26ac9baec291df296f5cc183c4227a9e1a42a5
valendion <valendion874@gmail.com>
2022-02-05 03:51:10 +0800

    Menambahkan library coroutine dan membuaat splash screen berjalan

--- app/src/main/java/com/example/wartajemaatapp/ui/SplashScreenActivity.kt
+++ app/src/main/java/com/example/wartajemaatapp/ui/SplashScreenActivity.kt
@@ -1,12 +1,38 @@
 package com.example.wartajemaatapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.wartajemaatapp.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.wartajemaatapp.databinding.ActivitySplashScreenBinding
+import kotlinx.coroutines.*
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+    val activityScope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Menggunakan tread main
+        activityScope.launch {
+            //Jeda 3 detik
+            delay(3000)
+
+            //Pindah activity dan destroy
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            finishAffinity()
+        }
     }
+
+    override fun onPause() {
+        // Membatalkan Coroutine
+        activityScope.cancel()
+        super.onPause()
+    }
+
+
 }
